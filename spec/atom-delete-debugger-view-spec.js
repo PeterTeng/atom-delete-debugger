449c2be535dca490a6e27d906461108d96d887a9
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2017-06-10 02:00:51 +0900

    Initial commit

---
+++ spec/atom-delete-debugger-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDeleteDebuggerView from '../lib/atom-delete-debugger-view';
+
+describe('AtomDeleteDebuggerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
